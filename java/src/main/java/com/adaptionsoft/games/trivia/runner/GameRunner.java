6cfdc2abf2e4880b35dd86ec5128757b06a33a36
SarrMoha <152873520+SarrMoha@users.noreply.github.com>
2023-12-06 16:11:23 +0100

    Update GameRunner.java

--- java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -1,40 +1,54 @@
-
 package com.adaptionsoft.games.trivia.runner;
+
 import java.util.Random;
 
 import com.adaptionsoft.games.uglytrivia.Game;
-
+import com.adaptionsoft.games.uglytrivia.Player;
 
 public class GameRunner {
 
-	private static boolean notAWinner;
+	//TODO : Use constants for values such as the number of sides of the die (5) and the threshold for triggering a wrong answer (7).
+
+	//TODO : Add comments that explain the purpose of each section of the code, especially in the playGame method
 
 	public static void main(String[] args) {
+		/*Rather than passing the random generator with each method call, we can add a constructor to the Game class that initializes the random generator.*/
 		Random rand = new Random();
 		playGame(rand);
-		
 	}
 
 	public static void playGame(Random rand) {
+
+		/*
+		* Avoid using a static variable (notAWinner). We can instead return a value from the playGame method.
+		* */
+
 		Game aGame = new Game();
+		boolean gameInProgress = true;
 
-		aGame.add("Chet");
-		aGame.add("Pat");
-		aGame.add("Sue");
+		aGame.addPlayer(new Player("Chet"));
+		aGame.addPlayer(new Player("Pat"));
+		aGame.addPlayer(new Player("Sue"));
 
+		aGame.setCurrentPlayer(aGame.getPlayers().get(0));
 
 		do {
-
-			aGame.roll(rand.nextInt(5) + 1);
+			int roll = rand.nextInt(5) + 1;
+			aGame.roll(roll);
 
 			if (rand.nextInt(9) == 7) {
-				notAWinner = aGame.wrongAnswer();
+				aGame.wrongAnswer(true);
 			} else {
-				notAWinner = aGame.wasCorrectlyAnswered();
+				aGame.wasCorrectlyAnswered(true);
 			}
 
+			if (aGame.isAnyWinner() != null) {
+				Player winner = aGame.isAnyWinner();
+				System.out.println(winner.getName() + " won the game!");
+				gameInProgress = false;
+			}
 
+		} while (gameInProgress);
 
-		} while (notAWinner);
 	}
 }
